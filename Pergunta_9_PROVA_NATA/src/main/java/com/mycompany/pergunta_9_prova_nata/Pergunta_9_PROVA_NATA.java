97c0064263eedf2473f5034b37b076aa9fb324b9
Natã Jordão <141784916+333NNN@users.noreply.github.com>
2024-10-25 21:17:38 -0300

    Pergunta 9 - Cálculo salário

---
+++ Pergunta_9_PROVA_NATA/src/main/java/com/mycompany/pergunta_9_prova_nata/Pergunta_9_PROVA_NATA.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.pergunta_9_prova_nata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ncunha
+ */
+public class Pergunta_9_PROVA_NATA {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Cálculo salarial\nInsira o valor de sua hora trabalhada: ");
+        float valorHora = read.nextFloat();
+
+        System.out.println("Insira o total de horas trabalhadas no mês: ");
+        int horaTrab = read.nextInt();
+
+        float salarioBruto = horaTrab * valorHora;
+        float impostoRenda = salarioBruto * 0.11f;
+        float inss = salarioBruto * 0.08f;
+        float sindicato = salarioBruto * 0.05f;
+        float salarioLiq = salarioBruto - impostoRenda - inss - sindicato;
+
+        System.out.println("+ Salário Bruto: R$ " + salarioBruto + "\n"
+                + "- Imposto de Renda (11%): R$ " + impostoRenda + "\n"
+                + "- INSS (8%): R$" + inss + "\n"
+                + "- Sindicato (5%): R$ " + sindicato + "\n"
+                + "= Salário Líquido: R$ " +  salarioLiq);
+    }
+}
